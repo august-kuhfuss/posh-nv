182d014b034cb8640b5c754e4daf22d1c0237b48
cethien <borislaw.sotnikow@gmx.de>
2024-03-20 11:49:27 +0100

    perf: compile

--- compile.ps1
+++ compile.ps1
@@ -1,36 +1,46 @@
+#Requires -Version 7
 param(
-    [Parameter(Mandatory)]
-    [string[]]$Packages
+    [string[]]$PackageNames
 )
 
-$configFile = "$PSScriptRoot\config.toml"
+$cli = $(Get-Childitem -Path "$env:ProgramFiles\Framework Systems\" -Recurse -Force "FSConsole.exe" -ErrorAction SilentlyContinue)
+$configFile = "$PSScriptRoot\config.json"
 if ((Test-Path $configFile) -eq $false) {
-    Write-Output "config.toml not found. exiting"
+    Write-Output "config.json not found. exiting"
     return
 }
-$config = Get-Content $configFile | ConvertFrom-Toml
+$config = Get-Content $configFile | ConvertFrom-Json
 
-$cli = $(Get-Childitem -Path "$env:ProgramFiles\Framework Systems\" -Recurse -Force "FSConsole.exe" -ErrorAction SilentlyContinue)
-
-$repo = $config.repo
+$repo = $config.repository
 $repoArgs = @(
     "\SERVER", $repo.host,
-    "\Database", $repo.database,
-    "\DBUser", $repo.user,
+    "\Database", $repo.database_name,
+    "\DBUser", $repo.username,
     "\DBPassword", $repo.password
     "\ConnectionType", "SqlServer"
 )
 
+# compile all if nothing is specified
+if ($null -eq $PackageNames) {
+    $PackageNames = $config.packages.name
+}
 
-$packages = $config.packages
-$Packages | ForEach-Object -Parallel {
-    $p = $($using:packages) | Where-Object { $_.name -eq $c } | Select-Object -First 1
+$packages = $config.packages | Where-Object {
+    $PackageNames -match $_.name
+}
 
+# if no matches, error
+if ($packages.Count -eq 0) {
+    Write-Output "package(s) `"$($PackageNames -join ", ")`" not found. exiting"
+    return
+}
+
+$packages | ForEach-Object -Parallel {
+    $package = $PSItem
     $compileArgs = $($using:repoArgs) + @(
-        "\LabelID", $p.labelId
+        "\LabelID", $package.label_id
         "\Compile"
     )
     $compile = Start-Process -FilePath $($using:cli) -ArgumentList $compileArgs -NoNewWindow -PassThru -Wait
-
     Write-Output "Process Ended with Code $($compile.ExitCode)"
 }
