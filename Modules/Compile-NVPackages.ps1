be6b33856f8d3d56be6213b223fe085fe537887d
cethien <borislaw.sotnikow@kuhfussonline.com>
2024-04-15 15:16:36 +0200

    fix: compile

--- Modules/Compile-NVPackages.ps1
+++ Modules/Compile-NVPackages.ps1
@@ -14,6 +14,14 @@ function Compile-NVPackages {
         $PackageNames -match $_.name
     }
 
+    $matched = $matched | Select-Object -Unique @{
+        Name       = 'PackageName'
+        Expression = { $_.package.name }
+    }, @{
+        Name       = 'VersionName'
+        Expression = { $_.version.name }
+    }
+
     # if no matches, error and exit
     if ($matched.Count -eq 0) {
         Write-Output "package(s) `"$($PackageNames -join ", ")`" not found. exiting"
@@ -23,14 +31,14 @@ function Compile-NVPackages {
     $cli = Get-FSConsole
     $repoArgs = Get-FSRepoArgs
     $matched | ForEach-Object -Parallel {
-        $package = $PSItem
+        $p = $PSItem
 
-        $compileArgs = $($using:repoArgs) + @(
-            "\PACKAGE", $package.label_id,
-            "\VERSION", $package.versions[0].name,
+        $compileArgs = $using:repoArgs + @(
+            "\PACKAGE", $p.PackageName,
+            "\VERSION", $p.VersionName,
             "\COMPILE"
         )
-        $compile = Start-Process -FilePath $($using:cli) -ArgumentList $compileArgs -NoNewWindow -PassThru -Wait
+        $compile = Start-Process -FilePath $using:cli -ArgumentList $compileArgs -NoNewWindow -PassThru -Wait
         Write-Output "Process Ended with Code $($compile.ExitCode)"
     }
 }
